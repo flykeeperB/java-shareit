847a1cafb2d4e69e866c031c3fcaff714a6dd437
Flykeeper <flykeeper@yandex.ru>
2023-09-29 23:40:43 +0500

    refactor: add docker (fix 10)

--- server/src/main/java/ru/practicum/shareit/item/mapping/ItemMapper.java
+++ server/src/main/java/ru/practicum/shareit/item/mapping/ItemMapper.java
@@ -1,10 +1,8 @@
 package ru.practicum.shareit.item.mapping;
 
 import ru.practicum.shareit.booking.mapping.BookingMapper;
-import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemExtraDto;
-import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
